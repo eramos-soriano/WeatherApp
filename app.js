462e71bc1b072c2838ba06fba5624387f187b51e
eramos-soriano <eramossorian@gmail.com>
2023-06-06 15:17:13 -0700

    editing html and js files

--- app.js
+++ app.js
@@ -74,7 +74,8 @@ function convertCountryCode(country){
 
 // get Weather function
 function getWeather(){
-    const API_KEY = 'ad30032a9503f46444f727be01a40021'
+    // add api key from openweathermap API
+    const API_KEY = ''
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => {
     console.log(data)
